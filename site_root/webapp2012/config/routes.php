80607cd830058b31a0bce6c56e76df2dc7a5454f
Joe Savona <joe@phonogram.co.jp>
2012-03-05 10:59:44 +0900

    routes: update example config/routes.php to use the abbreviated RESTful shorcut but provide documentation about what routes it expands to form.

--- site_root/webapp2012/config/routes.php
+++ site_root/webapp2012/config/routes.php
@@ -6,6 +6,10 @@ function defineRoutes($router)
 	$router->setDefaultController('SampleController');
 	$router->setErrorController('ErrorController');
 
+
+	$router->mapRest('sample', '/sample', 'SampleController');
+	/*	上記の一行は、下記の８ルートとなります：
+
 	$router->map('sample_index', 		'GET /sample', 				'SampleController', 'index');
 	$router->map('sample_new_form',		'GET /sample/new', 			'SampleController', 'newForm');
 	$router->map('sample_new_save',		'POST /sample/new', 		'SampleController', 'newSave');
@@ -14,26 +18,5 @@ function defineRoutes($router)
 	$router->map('sample_edit_save',	'POST /sample/#id/edit', 	'SampleController', 'editSave');
 	$router->map('sample_delete_form',	'GET /sample/#id/delete', 	'SampleController', 'deleteForm');
 	$router->map('sample_delete_save',	'POST /sample/#id/delete', 	'SampleController', 'deleteSave');
-
-	//	上記と全く同じルートを一気に作る
-	//$router->mapRest('sample', '/restSample', 'SampleController');
+	*/
 }
-
-/*
-function defineRoutes($router)
-{
-	$router->setSubDir('/webapp2012/');
-	$router->setDefaultController('SampleController');
-	$router->setDefaultAction('index');
-	$router->set404Controller('ErrorController');
-
-	//SampleController->index() (ディフォルトで)
-	$router->map('/sample');
-
-	//SampleController->show() (設定で)
-	$router->map('/sample/show', 'SampleController', 'show');
-
-	//SampleController->show(), $_GET['id']はURLから読み取る。URLの:id部分は数字ではないとルールに一致しない
-	$router->map('/sample/show/:id', 'SampleController', 'show', array(), array('id' => '[0-9]+'));
-}
-*/
