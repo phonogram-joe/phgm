8b962b7b3bbbdcef7d4078e91fbd9cfab487c76d
Joe Savona <joe@phonogram.co.jp>
2012-03-06 10:06:59 +0900

    sample: add copyright to files that were missing it

--- site_root/webapp2012/config/app_prod.php
+++ site_root/webapp2012/config/app_prod.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ *	Copyright (C) Phonogram Inc 2012
+ *	Licensed Under the MIT license http://www.opensource.org/licenses/mit-license.php
+ */
 
 //	このシステムの動きに関する設定はここで記入してください。
 //	このファイルは＊＊公開環境＊＊のみで有効です
