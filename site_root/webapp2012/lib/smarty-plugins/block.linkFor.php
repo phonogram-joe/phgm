151232e13552bf7b5d3147ab4519ecb9ce429505
Joe Savona <joe@phonogram.co.jp>
2012-03-15 18:11:52 +0900

    views: smarty block helper for generating links to routes.

---
+++ site_root/webapp2012/lib/smarty-plugins/block.linkFor.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ *	Copyright (C) Phonogram Inc 2012
+ *	Licensed Under the MIT license http://www.opensource.org/licenses/mit-license.php
+ */
+
+function smarty_block_linkFor($params, $content, Smarty_Internal_Template $template, &$repeat)
+{
+	if ($repeat) {
+		$name = array_key_exists('name', $params) ? $params['name'] : null;
+		unset($params['name']);
+		$html_id = array_key_exists('html_id', $params) ? $params['html_id'] : null;
+		unset($params['html_id']);
+		$html_class = array_key_exists('html_class', $params) ? $params['html_class'] : null;
+		unset($params['html_class']);
+
+		$url = Router::getRouter()->urlForName($name, $params);
+
+		$html  = '<a name="' . $name . '" href="' . $url . '" id="' . htmlspecialchars($html_id) . '" class="' . htmlspecialchars($html_class) . '">';
+	} else {
+		$html = $content . '</a>';
+	}
+	return $html;
+}
