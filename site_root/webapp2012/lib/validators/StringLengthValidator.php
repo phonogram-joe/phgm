2ab0379c37db2c90a57fe709e4373898bc75e51f
Joe Savona <joe@phonogram.co.jp>
2012-04-16 11:57:29 +0900

    validators: adjustments to required validator and add a string length validator (min/max)

---
+++ site_root/webapp2012/lib/validators/StringLengthValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+class StringLengthValidator
+{
+	public static function string_length($value, $min, $max)
+	{
+		if (is_null($value)) {
+			return null;
+		}
+		if (mb_strlen($value) < intval($min) || mb_strlen($value) > intval($max)) {
+			return $min . '-' . $max . '文字でお願いします。';
+		}
+		return null;
+	}
+}
