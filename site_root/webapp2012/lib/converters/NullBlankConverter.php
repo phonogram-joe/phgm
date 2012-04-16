7fa5393e2108eff12731bc7585c31ab600b5644c
Joe Savona <joe@phonogram.co.jp>
2012-04-16 12:00:49 +0900

    converters: trim strings in the NullBlank converter. was almost always using trim and null_blank together

--- site_root/webapp2012/lib/converters/NullBlankConverter.php
+++ site_root/webapp2012/lib/converters/NullBlankConverter.php
@@ -4,7 +4,7 @@ class NullBlankConverter
 {
 	public static function input($value)
 	{
-		return mb_strlen($value) === 0 || mb_strlen(trim($value)) === 0 ? null : $value;
+		return mb_strlen($value) === 0 || mb_strlen(trim($value)) === 0 ? null : trim($value);
 	}
 
 	public static function output($value)
